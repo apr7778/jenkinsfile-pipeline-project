9acc249f0ca5b6edbca089059cc917430a725cce
EC2 Default User <ec2-user@ip-172-31-64-109.ec2.internal>
2024-08-23 17:29:24 +0000

    push

--- pipeline.py
+++ pipeline.py
@@ -4,7 +4,8 @@ pipeline {
         stage('run') {
             steps {
                 echo 'Clarusway_Way to Reinvent Yourself'
-                sh 'python3 --version'
+                sh 'python3 --version'i
+                whoami
                 sh 'python3 pipeline.py'
             }
         }
